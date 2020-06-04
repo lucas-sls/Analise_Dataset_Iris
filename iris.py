14cc89903fad0d7face63663aedc3a171d68f711
Lucas Fonseca Muller <lucasfm2010@hotmail.com>
2020-06-04 01:20:07 -0300

    Inserido alguns comandos para exploração de dados e iniciada a visualização do Dataset com a plotagem do primeiro boxplot.

--- iris.py
+++ iris.py
@@ -1,6 +1,9 @@
+# Carregando libraries
+
 import pandas
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
@@ -11,3 +14,43 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+
+
+# CARGA DE DADOS
+
+# Link do arquivo csv
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+
+# Definindo o nome da coluna dos atributos
+names = ['sepal-length', 'sepal-width', 'petal-length','petal-width','class']
+
+# Carregando dataset com a url armazenada e com as colunas nomeadas
+df = pandas.read_csv(url, names=names)
+
+
+# ANÁLISE EXPLORATÓRIA
+
+# Número de linhas e colunas
+print(df.shape) # 150 linhas e 5 colunas
+
+# Mostra primeiras 15 linhas
+print(df.head(15))
+
+# Resumo dos atributos
+print(df.describe())
+
+# Distribuição por classe
+print(df.groupby('class').size()) # 50, 50 e 50
+
+
+# VISUALIZAÇÃO DOS DADOS
+
+#Gráficos de uma variável
+
+sns.set_style('darkgrid')
+sns.boxplot(x='class',y='sepal-length', data=df)
+plt.title("Boxplot: Comp. de Sépala VS Espécies de Iris", fontsize=16)
+plt.xlabel("Espécies de Iris", fontsize=12)
+plt.ylabel("Comprimento de Sépala (cm)" , fontsize=12)
+plt.show()
+
