bf009fee4024757ec0dbe4c741063a3567684c47
Lucas Fonseca Muller <lucasfm2010@hotmail.com>
2020-06-06 02:25:07 -0300

    Construção dos scatters-plots com KDE, histograma, barplots e boxplots comparativos.

--- iris.py
+++ iris.py
@@ -3,6 +3,7 @@
 import pandas
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
+
 import seaborn as sns
 from sklearn import model_selection
 from sklearn.metrics import classification_report
@@ -18,92 +19,76 @@ from sklearn.svm import SVC
 
     # CARGA DE DADOS
 
-# Link do arquivo csv
+    # Link do arquivo csv
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 
-# Definindo o nome da coluna dos atributos
+    # Definindo o nome da coluna dos atributos
 names = ['sepal-length', 'sepal-width', 'petal-length','petal-width','class']
 
-# Carregando dataset com a url armazenada e com as colunas nomeadas
+    # Carregando dataset com a url armazenada e com as colunas nomeadas
 df = pandas.read_csv(url, names=names)
 
-# Criando Dataframes por classe de íris, para análises futuras
+    # Criando Dataframes por classe de íris, para análises futuras
 df_setosa = df[df['class'] == 'Iris-setosa']
 df_versicolor = df[df['class'] == 'Iris-virginica']
 df_virginica = df[df['class'] == 'Iris-versicolor']
 
 
-# ANÁLISE EXPLORATÓRIA
+    # ANÁLISE EXPLORATÓRIA
 
     # Número de linhas e colunas
-#print(df.shape) # 150 linhas e 5 colunas
+print(df.shape) # 150 linhas e 5 colunas
 
     # Mostra primeiras 15 linhas
-#print(df.head(15))
+print(df.head(15))
 
     # Resumo dos atributos
-#print(df.describe())
+print(df.describe())
 
     # Distribuição por classe
-#print(df.groupby('class').size()) # 50, 50 e 50
+print(df.groupby('class').size()) # 50, 50 e 50
 
 
 # VISUALIZAÇÃO DOS DADOS
 
-    #Boxplots
-
-# sns.set_style('darkgrid')
-# sns.boxplot(x='class',y='sepal-length', data=df)
-# plt.title("Boxplot: Comp. da Sépala VS Espécies de Iris", fontsize=16)
-# plt.xlabel("Espécies de Iris", fontsize=12)
-# plt.ylabel("Comprimento da Sépala (cm)" , fontsize=12)
-# plt.show()
-
-# sns.set_style('darkgrid')
-# sns.boxplot(x='class',y='sepal-width', data=df)
-# plt.title("Boxplot: Larg. da Sépala VS Espécies de Iris", fontsize=16)
-# plt.xlabel("Espécies de Iris", fontsize=12)
-# plt.ylabel("Largura da Sépala (cm)" , fontsize=12)
-# plt.show()
-
-# sns.set_style('darkgrid')
-# sns.boxplot(x='class',y='petal-length', data=df)
-# plt.title("Boxplot: Comp. da Pétala VS Espécies de Iris", fontsize=16)
-# plt.xlabel("Espécies de Iris", fontsize=12)
-# plt.ylabel("Comprimento da Pétala (cm)" , fontsize=12)
-# plt.show()
-
-# sns.set_style('darkgrid')
-# sns.boxplot(x='class',y='sepal-width', data=df)
-# plt.title("Boxplot: Larg. da Pétala VS Espécies de Iris", fontsize=16)
-# plt.xlabel("Espécies de Iris", fontsize=12)
-# plt.ylabel("Largura da Sépala (cm)" , fontsize=12)
-# plt.show()
-
-
-    #Histogramas
+    #Scatter-Plot + KDE
 sns.set_style('darkgrid')
-sns.distplot(a=df_setosa['sepal-length'])
+sns.pairplot(df, hue='class', height=2.5, palette='Set2', diag_kind='kde' )
 plt.show()
 
-sns.distplot(a=df_setosa['petal-length'])
+     #Histograma dos atributos quantitativos
+sns.set_style('darkgrid')
+df.hist(bins=7)
 plt.show()
 
-sns.distplot(a=df_setosa['sepal-width'])
+    #Countplot das espécies de íris
+sns.set_style('darkgrid')
+sns.countplot(x='class',data=df, palette='Set2')
+plt.title('Quantidade de amostras por Espécie de Iris', fontsize=16)
+plt.xlabel('Espécies de Iris', fontsize=13)
+plt.ylabel('Quantidade',fontsize=13)
 plt.show()
 
-sns.distplot(a=df_setosa['petal-width'])
+    #Boxplots das variáveis quantitativas
+sns.set_style('darkgrid')
+fig = plt.figure()
+fig.subplots_adjust(hspace=0.5, wspace=0.3)
+ax = fig.add_subplot(2,2,1)
+plt.title('Comp. de Sépala VS Tipo de Iris')
+sns.boxplot(x='class',y='sepal-length', data=df, ax=ax)
+ax = fig.add_subplot(2,2,2)
+plt.title('Comp. de Pétala VS Tipo de Iris')
+sns.boxplot(x='class',y='petal-length', data=df, ax=ax)
+ax = fig.add_subplot(2,2,3)
+plt.title('Larg. de Sépala VS Tipo de Iris')
+sns.boxplot(x='class',y='sepal-width', data=df, ax=ax)
+ax = fig.add_subplot(2,2,4)
+plt.title('Larg. de Pétala VS Tipo de Iris')
+sns.boxplot(x='class',y='petal-width', data=df, ax=ax)
+
 plt.show()
 
-# sns.set_style('whitegrid')
-# sns.distplot(a=df['sepal-width'])
-# plt.show()
 
-# sns.set_style('whitegrid')
-# sns.distplot(a=df['petal-length'])
-# plt.show()
-#
-# sns.set_style('whitegrid')
-# sns.distplot(a=df['petal-width'])
-# plt.show()
+
+
 
